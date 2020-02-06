e6f1fe7aa0dcc28bb389686e3109865594a1c6d6
Gabriel Galán <gabrielgalan@hotmail.com>
2020-02-06 11:31:24 -0600

    Deleting duplicated files.

--- users_app/views.py
+++ users_app/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm # Helper to create forms in DJANGO.
+from django.contrib import messages
 
 def register(request):
-    return HttpResponse('users_app working')
+    if request.method == "POST":
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid(): # If the information in request.POST is valid, do all the following.
+            register_form.save() # Save the register account in database.
+            messages.success(request, ("New User Account Created, Login To Get Started!")) # If register_form is valid, show a success message.
+            return redirect('register')
+    else: # If the request is a "GET", do all the following.
+        register_form = UserCreationForm()
+    return render(request, 'register.html', {'register_form': register_form})
