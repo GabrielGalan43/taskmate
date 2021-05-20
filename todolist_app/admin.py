0abea06d23c4891915a6d76f2414660fd3a832d2
Gabriel <gabrielgalan@hotmail.com>
2021-05-19 23:03:58 -0500

    Upgrade requirements.txt and todo_list admin.py modified

--- todolist_app/admin.py
+++ todolist_app/admin.py
@@ -3,4 +3,7 @@ from todolist_app.models import TaskList
 
 
 # Here we are registering the database model that we've created.
-admin.site.register(TaskList) 
+class TaskListAdmin(admin.ModelAdmin):
+    list_display = ['task', 'done',]
+    
+admin.site.register(TaskList, TaskListAdmin)
